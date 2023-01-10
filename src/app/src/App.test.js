034b3945bac182d38aca56f077f1cde1f24c847f
Tripp Shelnutt <trippshelnutt@outlook.com>
2023-01-10 08:02:29 -0500

    Removed create react app default files

--- src/app/src/App.test.js
+++ src/app/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerTitle = screen.getByText(/Transaction Sorter/i);
+  expect(headerTitle).toBeInTheDocument();
 });
