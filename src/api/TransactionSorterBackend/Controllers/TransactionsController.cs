4bb9301c01417bf34f05a6de50d8d3d7b28b6a56
Tripp Shelnutt <trippshelnutt@outlook.com>
2023-01-30 21:42:09 -0500

    Added payee to example transactions

--- src/api/TransactionSorterBackend/Controllers/TransactionsController.cs
+++ src/api/TransactionSorterBackend/Controllers/TransactionsController.cs
@@ -21,12 +21,14 @@ public class TransactionsController : ControllerBase
             {
                 MilliunitAmount = 12340,
                 Date = new DateTime(year, month, 1),
+                Payee = "First",
                 ParentTransactionId = "12340"
             },
             new TransactionModel
             {
                 MilliunitAmount = 123450,
                 Date = new DateTime(year, month, 2),
+                Payee = "Second",
                 ParentTransactionId = "123450"
             },
             new TransactionModel
@@ -34,6 +36,7 @@ public class TransactionsController : ControllerBase
 
                 MilliunitAmount = 1234560,
                 Date = new DateTime(year, month, 3),
+                Payee = "Third",
                 ParentTransactionId = "1234560"
             },
             new TransactionModel
@@ -41,12 +44,14 @@ public class TransactionsController : ControllerBase
 
                 MilliunitAmount = 12345670,
                 Date = new DateTime(year, month, 4),
+                Payee = "Fourth",
                 ParentTransactionId = "12345670"
             },
             new TransactionModel
             {
                 MilliunitAmount = 123456780,
                 Date = new DateTime(year, month, 5),
+                Payee = "Fifth",
                 ParentTransactionId = "123456780"
             }
         };
