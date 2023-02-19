f683b71b0a0b51e46ecf96030d4f84bc10ca93a0
Tripp Shelnutt <trippshelnutt@outlook.com>
2023-02-19 11:03:48 -0500

    Recreated API using AWS Lambda tools

--- src/api/TransactionSorterBackend/Program.cs
+++ src/api/TransactionSorterBackend/Program.cs
@@ -1,25 +1,32 @@
+const string TransactionSorterSpecificOrigins = "TransactionSorterSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: TransactionSorterSpecificOrigins,
+    policy =>
+    {
+        policy.WithOrigins("*");
+    });
+});
 
+// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Add AWS Lambda support. When application is run in Lambda Kestrel is swapped out as the web server with Amazon.Lambda.AspNetCoreServer. This
+// package will act as the webserver translating request and responses between the Lambda event source and ASP.NET Core.
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
+app.UseCors(TransactionSorterSpecificOrigins);
+
 app.MapControllers();
+
+app.MapGet("/", () => "Welcome to running ASP.NET Core Minimal API on AWS Lambda");
 
 app.Run();
